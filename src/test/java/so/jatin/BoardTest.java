9fe0c28943111fe44b5eea17279fb53ade748095
Jatin Sonavane <j@jatin.so>
2013-10-04 15:59:30 -0700

    Test board for multiple exits - works as is.

--- src/test/java/so/jatin/BoardTest.java
+++ src/test/java/so/jatin/BoardTest.java
@@ -9,19 +9,42 @@ public class BoardTest {
 
 	private Board subject;
 
-	@Before
-	public void setUp() {
-		subject = new Board(7, 7);		
-	}
-
 	@Test
-	public void test() {
+	public void oneExit() {
+		subject = new Board(7, 7);		
 		subject.addExit(5, 3);
 		assertEquals(0, subject.getCost(5, 3));
 		assertEquals(1, subject.getCost(6, 3));
 		assertEquals(2, subject.getCost(6, 4));
 		assertEquals(5, subject.getCost(0, 3));
 		assertEquals(5, subject.getCost(2, 1));
+	}
+
+	@Test
+	public void manyExits() {
+		subject = new Board(5, 5);
+		subject.addExit(1,  0);
+		subject.addExit(2,  3);
+		subject.addExit(4,  0);
+		/*
+		 * Costs should look like this (top left is (0,0)):
+		 * 1 0 1 1 0
+		 * 2 1 2 2 1
+		 * 3 2 1 2 2
+		 * 2 1 0 1 2
+		 * 3 2 1 2 3
+		 */
+		assertEquals(0, subject.getCost(1, 0));
+		assertEquals(0, subject.getCost(2, 3));
+		assertEquals(0, subject.getCost(4, 0));
+		assertEquals(1, subject.getCost(0, 0));
+		assertEquals(1, subject.getCost(2, 0));
+		assertEquals(2, subject.getCost(0, 1));
+		assertEquals(2, subject.getCost(3, 1));
+		assertEquals(3, subject.getCost(0, 2));
+		assertEquals(1, subject.getCost(3, 3));
+		assertEquals(2, subject.getCost(4, 2));
+
 	}
 
 }
