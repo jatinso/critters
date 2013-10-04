a72acc9cc0f799216d9b0b91e53a08657df312b2
Jatin Sonavane <j@jatin.so>
2013-10-04 16:00:51 -0700

    I no longer need the single exit test.

--- src/test/java/so/jatin/BoardTest.java
+++ src/test/java/so/jatin/BoardTest.java
@@ -1,24 +1,12 @@
 package so.jatin;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class BoardTest {
 
 	private Board subject;
-
-	@Test
-	public void oneExit() {
-		subject = new Board(7, 7);		
-		subject.addExit(5, 3);
-		assertEquals(0, subject.getCost(5, 3));
-		assertEquals(1, subject.getCost(6, 3));
-		assertEquals(2, subject.getCost(6, 4));
-		assertEquals(5, subject.getCost(0, 3));
-		assertEquals(5, subject.getCost(2, 1));
-	}
 
 	@Test
 	public void manyExits() {
