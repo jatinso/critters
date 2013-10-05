53274d6149aecba836846b977a3cb6d1f3ed0d86
Jatin Sonavane <j@jatin.so>
2013-10-04 18:28:27 -0700

    Now we can add obstacles to the board.

--- src/main/java/so/jatin/Board.java
+++ src/main/java/so/jatin/Board.java
@@ -14,6 +14,8 @@ import java.util.Queue;
 public class Board {
 	
 	private final int[][] cost;
+	private static final int OBSTACLE = -2;
+	private static final int INFINITY = -1;
 
 	public Board(int width, int height) {
 
@@ -22,7 +24,7 @@ public class Board {
 		// At first there are no exits. So -1 indicates there's no route from here to an exit.
 		for (int x = 0; x < width; x++)
 			for (int y = 0; y < height; y++)
-				cost[x][y] = -1;
+				cost[x][y] = INFINITY;
 	}
 
 	public void addExit(int x, int y) {
@@ -30,57 +32,108 @@ public class Board {
 		// update their costs.
 		cost[x][y] = 0;
 		
-		Queue<Coordinate> queue = new LinkedList<Coordinate>();
-		queue.add(new Coordinate(x, y));
+		Queue<Point> queue = new LinkedList<Point>();
+		queue.add(new Point(x, y));
 		
 		// This is a BFS that propagates out the cost changes due to the creation
 		// of the exit cell.
 		while (!queue.isEmpty()) {
-			Coordinate coordinate = queue.remove();
-			int myCost = cost[coordinate.x][coordinate.y];
+			Point point = queue.remove();
+			int myCost = cost[point.x][point.y];
 			
-			for (Coordinate neighbour : getNeighbours(coordinate)) {
-				int neighbourCost = cost[neighbour.x][neighbour.y];
+			for (Point neighbor : getNeighbors(point)) {
+				int neighborCost = cost[neighbor.x][neighbor.y];
 				
 				// Only if a neighbour is impacted, add it to the queue
-				if (neighbourCost == -1 || neighbourCost > myCost + 1) {
-					cost[neighbour.x][neighbour.y] = myCost + 1;
-					queue.add(neighbour); // propagate your changes!
+				if (neighborCost == INFINITY || neighborCost > myCost + 1) {
+					cost[neighbor.x][neighbor.y] = myCost + 1;
+					queue.add(neighbor); // propagate your changes!
 				}
 			}
 		}
 	}
 
-	private List<Coordinate> getNeighbours(Coordinate coordinate) {
-		List<Coordinate> neighbours = new ArrayList<Board.Coordinate>();
+	private List<Point> getNeighbors(Point point) {
+		List<Point> neighbours = new ArrayList<Board.Point>();
 
-		if (coordinate.x > 0)
-			neighbours.add(new Coordinate(coordinate.x - 1, coordinate.y));
+		// An obstacle is not considered as a valid neighbour.
+		if (point.x > 0 && !isBlocked(point.x - 1, point.y))
+			neighbours.add(new Point(point.x - 1, point.y));
 
-		if (coordinate.y > 0)
-			neighbours.add(new Coordinate(coordinate.x, coordinate.y - 1));
+		if (point.y > 0 && !isBlocked(point.x, point.y - 1))
+			neighbours.add(new Point(point.x, point.y - 1));
 
-		if (coordinate.x < cost.length - 1)
-			neighbours.add(new Coordinate(coordinate.x + 1, coordinate.y));
+		if (point.x < cost.length - 1 && !isBlocked(point.x + 1, point.y))
+			neighbours.add(new Point(point.x + 1, point.y));
 
-		if (coordinate.y < cost[0].length - 1)
-			neighbours.add(new Coordinate(coordinate.x, coordinate.y + 1));
+		if (point.y < cost[0].length - 1 && !isBlocked(point.x, point.y + 1))
+			neighbours.add(new Point(point.x, point.y + 1));
 
 		return neighbours;
+	}
+
+	public void addObstacle(int x, int y) {
+		/*
+		 * When we add an obstacle, downstream cells (those with cost one higher than
+		 * this cell) may need to recompute their costs. And if they change, then
+		 * so would their downstream cells, and so on.
+		 */
+		Queue<Point> queue = new LinkedList<Point>();
+		queue.addAll(getDownstreamNeighbors(x, y));
+		cost[x][y] = OBSTACLE; // Note we have to update the cost AFTER the call to getDownstreamNeighbors().
+		
+		while (!queue.isEmpty()) {
+			Point point = queue.remove();
+			int updatedCost = getBestCost(point); // by looking at neighbors
+			if (getCost(point) != updatedCost) { // I have changed. So propagate.
+				queue.addAll(getDownstreamNeighbors(point.x, point.y));
+				cost[point.x][point.y] = updatedCost; // Note we have to update the cost AFTER the call to getDownstreamNeighbors().
+			}
+		}
+	}
+	
+	private int getBestCost(Point point) {
+
+		int bestCost = INFINITY;
+		for (Point neighbor : getNeighbors(point)) {
+			int neighborCost = getCost(neighbor);
+			if (bestCost == INFINITY || (neighborCost != INFINITY && bestCost > neighborCost))
+				bestCost = getCost(neighbor);
+		}
+
+		return bestCost + 1; // since it was best neighbor cost.
+	}
+
+	private List<Point> getDownstreamNeighbors(int x, int y) {
+		List<Point> downstreamNeighbors = new ArrayList<Point>();
+		
+		for (Point neighbor : getNeighbors(new Point(x, y)))
+			if (getCost(neighbor) == getCost(x, y) + 1)
+				downstreamNeighbors.add(neighbor);
+
+		return downstreamNeighbors;
+	}
+
+	private boolean isBlocked(int x, int y) {
+		return cost[x][y] == OBSTACLE;
 	}
 
 	public int getCost(int x, int y) {
 		return cost[x][y];
 	}
 
+	private int getCost(Point point) {
+		return getCost(point.x, point.y);
+	}
+
 	/*
 	 * This is a convenience class for holding x and y coordinates.
 	 */
-	private class Coordinate {
+	private class Point {
 		public final int x;
 		public final int y;
 		
-		public Coordinate(int x, int y) {
+		public Point(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
--- src/test/java/so/jatin/BoardTest.java
+++ src/test/java/so/jatin/BoardTest.java
@@ -9,30 +9,94 @@ public class BoardTest {
 	private Board subject;
 
 	@Test
-	public void manyExits() {
+	public void withObstaclesAddedAfterExits() {
 		subject = new Board(5, 5);
+
 		subject.addExit(1,  0);
 		subject.addExit(2,  3);
 		subject.addExit(4,  0);
-		/*
-		 * Costs should look like this (top left is (0,0)):
-		 * 1 0 1 1 0
-		 * 2 1 2 2 1
-		 * 3 2 1 2 2
-		 * 2 1 0 1 2
-		 * 3 2 1 2 3
-		 */
-		assertEquals(0, subject.getCost(1, 0));
-		assertEquals(0, subject.getCost(2, 3));
-		assertEquals(0, subject.getCost(4, 0));
-		assertEquals(1, subject.getCost(0, 0));
-		assertEquals(1, subject.getCost(2, 0));
-		assertEquals(2, subject.getCost(0, 1));
-		assertEquals(2, subject.getCost(3, 1));
-		assertEquals(3, subject.getCost(0, 2));
-		assertEquals(1, subject.getCost(3, 3));
-		assertEquals(2, subject.getCost(4, 2));
+
+		subject.addObstacle(2, 1);
+		subject.addObstacle(3, 2);
+		subject.addObstacle(1, 3);
+		subject.addObstacle(3, 3);
+		subject.addObstacle(2, 4);
+
+		assertBoardWithCertainObstaclesAndExits();
+	}
+	
+	@Test
+	public void withObstaclesAddedBeforeExits() {
+		subject = new Board(5, 5);
+
+		subject.addObstacle(2, 1);
+		subject.addObstacle(3, 2);
+		subject.addObstacle(1, 3);
+		subject.addObstacle(3, 3);
+		subject.addObstacle(2, 4);
+
+		subject.addExit(1,  0);
+		subject.addExit(2,  3);
+		subject.addExit(4,  0);
+	}
+	
+	@Test
+	public void withObstaclesAndExitsAddedIntermingled() {
+		subject = new Board(5, 5);
+
+		subject.addObstacle(2, 1);
+		subject.addExit(1,  0);
+		subject.addObstacle(3, 2);
+		subject.addObstacle(1, 3);
+		subject.addExit(2,  3);
+		subject.addObstacle(3, 3);
+		subject.addObstacle(2, 4);
+		subject.addExit(4,  0);
 
 	}
+	
+	private void assertBoardWithCertainObstaclesAndExits() {
+		/*
+		 * Costs should look like this (top left is (0,0)).
+		 * X represents an obstacle, which has cost -2. 
+		 * That just means there's no path from an obstacle to an exit.
+		 * 
+		 * 1 0 1 1 0
+		 * 2 1 X 2 1
+		 * 3 2 1 X 2
+		 * 4 X 0 X 3
+		 * 5 6 X 5 4
+		 */
+		
+		// Assert
+		assertEquals(1, subject.getCost(0, 0));
+		assertEquals(2, subject.getCost(0, 1));
+		assertEquals(3, subject.getCost(0, 2));
+		assertEquals(4, subject.getCost(0, 3));
+		assertEquals(5, subject.getCost(0, 4));
 
+		assertEquals(0, subject.getCost(1, 0));
+		assertEquals(1, subject.getCost(1, 1));
+		assertEquals(2, subject.getCost(1, 2));
+		assertEquals(-2, subject.getCost(1, 3));
+		assertEquals(6, subject.getCost(1, 4));
+
+		assertEquals(1, subject.getCost(2, 0));
+		assertEquals(-2, subject.getCost(2, 1));
+		assertEquals(1, subject.getCost(2, 2));
+		assertEquals(0, subject.getCost(2, 3));
+		assertEquals(-2, subject.getCost(2, 4));
+
+		assertEquals(1, subject.getCost(3, 0));
+		assertEquals(2, subject.getCost(3, 1));
+		assertEquals(-2, subject.getCost(3, 2));
+		assertEquals(-2, subject.getCost(3, 3));
+		assertEquals(5, subject.getCost(3, 4));
+
+		assertEquals(0, subject.getCost(4, 0));
+		assertEquals(1, subject.getCost(4, 1));
+		assertEquals(2, subject.getCost(4, 2));
+		assertEquals(3, subject.getCost(4, 3));
+		assertEquals(4, subject.getCost(4, 4));
+	}
 }
